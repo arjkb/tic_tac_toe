395e410864dd58e740794d51ce763914bdac3296
Arjun_Krishna_Babu <arjunkbabu@users.noreply.github.com>
2014-11-08 15:17:11 +0530

    Incorporated mouse functionality

--- tictac.java
+++ tictac.java
@@ -1,11 +1,19 @@
 /*
+	TIC-TAC-TOW PROJECT
+	(Program under development)
+
 	Main implementation of tic tac toe
+
+	OS		: Ubuntu 14.04 LTS
+	CODER	: Arjun Krishna Babu
+	GIT		: https://github.com/arjunkbabu/tic_tac_toe
+
 
 */
 
-import java.awt.*;
 import java.applet.*;
-
+import java.awt.*;
+import java.awt.event.*;
 /*
 <APPLET code = "tictac" width = 330 height = 330 >
 </APPLET>
@@ -15,6 +23,8 @@ public class tictac extends Applet	{
 	String msg;
 	public void init()	{
 		msg = "Hello";
+		addMouseListener(new MyMouseAdapter(this));
+		addMouseMotionListener(new MyMouseMotionAdapter(this));
 	}
 
 	public void update(Graphics g)	{
@@ -23,5 +33,29 @@ public class tictac extends Applet	{
 
 	public void paint(Graphics g)	{
 		update(g);
+	}
+}
+
+class MyMouseAdapter extends MouseAdapter	{
+	tictac T;
+
+	MyMouseAdapter(tictac T)	{
+		this.T = T;
+	}
+
+	public void mouseClicked(MouseEvent ME)	{
+		T.showStatus(" CLICK!");
+	}
+}
+
+class MyMouseMotionAdapter extends MouseMotionAdapter	{
+	tictac T;
+
+	MyMouseMotionAdapter(tictac T)	{
+		this.T = T;
+	}
+
+	public void mouseMoved(MouseEvent ME)	{
+		T.showStatus( " ( " + ME.getX() + ", " + ME.getY() + ")");
 	}
 }
