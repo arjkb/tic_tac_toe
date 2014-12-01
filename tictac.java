1f4303ff7cd24cd31f1ee6dbb567b8dfab3c31c0
Arjun_Krishna_Babu <arjunkbabu@users.noreply.github.com>
2014-12-01 21:56:56 +0530

    Array backbone implementation; probs with resizing solved

--- tictac.java
+++ tictac.java
@@ -30,6 +30,7 @@ public class tictac extends Applet	{
 	double v_end;	//ending y co-ordinate of vertical line
 
 	int lasthit = 0; //keeps track of which square was hit last (numbered as shown below)
+	int [][]A = new int[3][3];
 	/*
 		1|2|3
 		- - -
@@ -42,6 +43,10 @@ public class tictac extends Applet	{
 //		msg = "Hello";
 		addMouseListener(new MyMouseAdapter(this));
 		addMouseMotionListener(new MyMouseMotionAdapter(this));
+
+		for(int i = 0; i < 3; i++)
+			for(int j = 0; j < 3; j++)
+				A[i][j] = 0;
 	}
 
 	public int ptov(double perc, int targ)	{	//returns perc% of targ (in int)
@@ -73,34 +78,52 @@ public class tictac extends Applet	{
 		if( xx > ptov(0.06, dim.width) && xx < ptov(0.34, dim.width) )	{	//left coloumn
 			if( yy > ptov(0.06,  dim.height) && yy < ptov(0.34, dim.height) )	{
 				lasthit = 1;
+				A[0][0] = 1;
+				showStatus("SQUARE ONE");
 			}
 			else if( yy > ptov(0.37,  dim.height) && yy < ptov(0.64, dim.height) )	{
 				lasthit = 4;
+				A[1][0] = 1;
+				showStatus("SQUARE FOUR");
 			}
 			else if( yy > ptov(0.67,  dim.height) && yy < ptov(0.94, dim.height) )	{
 				lasthit = 7;
+				A[2][0] = 1;
+				showStatus("SQUARE SEVEN");
 			}
 		}
 		else if( xx > ptov(0.37, dim.width) && xx < ptov(0.64, dim.width) )	{ //middle coloumn
 			if( yy > ptov(0.06,  dim.height) && yy < ptov(0.34, dim.height) )	{
 				lasthit = 2;
+				A[0][1] = 1;
+				showStatus("SQUARE TWO");
 			}
 			else if( yy > ptov(0.37,  dim.height) && yy < ptov(0.64, dim.height) )	{
 				lasthit = 5;
+				A[1][1] = 1;
+				showStatus("SQUARE FIVE");
 			}
 			else if( yy > ptov(0.67,  dim.height) && yy < ptov(0.94, dim.height) )	{
 				lasthit = 8;
+				A[2][1] = 1;
+				showStatus("SQUARE EIGHT");
 			}
 		}
 		else if( xx > ptov(0.67, dim.width) && xx < ptov(0.92, dim.width) )	{ //right coloumn
 			if( yy > ptov(0.06,  dim.height) && yy < ptov(0.34, dim.height) )	{
 				lasthit = 3;
+				A[0][2] = 1;
+				showStatus("SQUARE THREE");
 			}
 			else if( yy > ptov(0.37,  dim.height) && yy < ptov(0.64, dim.height) )	{
 				lasthit = 6;
+				A[1][2] = 1;
+				showStatus("SQUARE SIX");
 			}
 			else if( yy > ptov(0.67,  dim.height) && yy < ptov(0.94, dim.height) )	{
 				lasthit = 9;
+				A[2][2] = 1;
+				showStatus("SQUARE NINE");
 			}
 		}			
 	}
@@ -131,7 +154,9 @@ public class tictac extends Applet	{
 		g.drawLine( (int) (0.65 * dim.width)	, (int) v_start, (int) (0.65 * dim.width)	 , (int) v_end ); 
 		g.drawLine( (int) (0.65 * dim.width + 1), (int) v_start, (int) (0.65 * dim.width + 1), (int) v_end ); 
 
+
 		int dx = 0, dy = 0;
+/*
 		switch(lasthit)	{ //resolve x and y co-ordinates of a particular square
 			case 1: dx = ptov(0.09, dim.width);
 					dy = ptov(0.09, dim.height);
@@ -161,9 +186,57 @@ public class tictac extends Applet	{
 					dy = ptov(0.68, dim.height);
 					break;
 		}
-		drawX(g, dx, dy);
+*/
+		if(A[0][0] == 1)	{
+			dx = ptov(0.09, dim.width);
+			dy = ptov(0.09, dim.height);
+			drawX(g, dx, dy);
+		}
+		if(A[0][1] == 1)	{
+			dx = ptov(0.38, dim.width);
+			dy = ptov(0.09, dim.height);
+			drawX(g, dx, dy);
+		}
+		if(A[0][2] == 1)	{
+			dx = ptov(0.68, dim.width);
+			dy = ptov(0.09, dim.height);
+			drawX(g, dx, dy);
+		}
+		if(A[1][0] == 1)	{
+			dx = ptov(0.09, dim.width);
+			dy = ptov(0.38, dim.height);
+			drawX(g, dx, dy);
+		}
+		if(A[1][1] == 1)	{
+			dx = ptov(0.38, dim.width);
+			dy = ptov(0.38, dim.height);
+			drawX(g, dx, dy);
+		}
+		if(A[1][2] == 1)	{
+			dx = ptov(0.68, dim.width);
+			dy = ptov(0.38, dim.height);
+			drawX(g, dx, dy);
+		}
+		if(A[2][0] == 1)	{
+			dx = ptov(0.09, dim.width);
+			dy = ptov(0.68, dim.height);
+			drawX(g, dx, dy);
+		}
+		if(A[2][1] == 1)	{
+			dx = ptov(0.38, dim.width);
+			dy = ptov(0.68, dim.height);
+			drawX(g, dx, dy);
+		}
+		if(A[2][2] == 1)	{
+			dx = ptov(0.68, dim.width);
+			dy = ptov(0.68, dim.height);
+			drawX(g, dx, dy);
+		}
+			
+			
+//		drawX(g, dx, dy);
 //		drawC(g, dx, dy);
-		showStatus(" Best if you do not resize ");
+//		showStatus(" Best if you do not resize ");
 	}
 
 	public void paint(Graphics g)	{
